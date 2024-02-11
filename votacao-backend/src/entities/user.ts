faf51e12576791fdab4fb534e3a9491edd1beb07
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-11 01:08:57 -0300

    update entities contructors for better database insertions

--- votacao-backend/src/entities/user.ts
+++ votacao-backend/src/entities/user.ts
@@ -2,16 +2,12 @@ import { Vote } from "./vote";
 
 export class User {
     id: number | undefined;
-    name: string;
-    email: string;
-    cpf: string;
+    name: string | undefined;
+    email: string | undefined;
+    cpf: string | undefined;
     votes: Vote[]
 
     private constructor(uid: number | undefined, name: string | undefined, email: string | undefined, cpf: string | undefined) {
-        if (!name || !email || !cpf) {
-            throw new Error('Invalid input values');
-        }
-
         this.id = uid;
         this.name = name;
         this.email = email;
@@ -20,14 +16,25 @@ export class User {
     }
 
     static create(name: string | undefined, email: string | undefined, cpf: string | undefined) {
+        if (!name || !email || !cpf) {
+            throw new Error('Invalid input values');
+        }
         return new User(undefined, name, email, cpf);
     }
 
     static existing(uid: number | undefined, name: string | undefined, email: string | undefined, cpf: string | undefined) {
         if (!uid) throw new Error('Invalid user Id');
+
+        if (!name || !email || !cpf) {
+            throw new Error('Invalid input values');
+        }
+
         return new User(uid, name, email, cpf);
     }
 
-
+    static fromId(id: number | undefined) {
+        if (!id) throw new Error('Invalid user Id');
+        return new User(id, '', '', '');
+    }
 
 }
