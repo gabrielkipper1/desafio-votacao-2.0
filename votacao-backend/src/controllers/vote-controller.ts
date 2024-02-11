e1595fbf5dc68fd39e4a49b512f5f5bd1b02778a
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-11 01:14:55 -0300

    update controllers parameters to use data interfaces instead of entity objects

--- votacao-backend/src/controllers/vote-controller.ts
+++ votacao-backend/src/controllers/vote-controller.ts
@@ -1,4 +1,8 @@
+import { User } from "../entities/user";
 import { Vote } from "../entities/vote";
+import { VotingTopic } from "../entities/voting-topic";
+import { VotePostData } from "../interfaces/vote-post-data";
+import { VotingResult } from "../interfaces/voting-result";
 import { VoteRepository } from "../repositories/interfaces/vote-repository";
 
 export class VoteController {
@@ -8,11 +12,17 @@ export class VoteController {
         this.repository = repository;
     }
 
-    async createVote(vote: Vote): Promise<Vote | undefined> {
+    async createVote(votePostData: VotePostData): Promise<Vote | undefined> {
+        const vote = Vote.create(User.fromId(votePostData.userId), VotingTopic.fromId(votePostData.topicId), votePostData.vote);
         return this.repository.saveVote(vote);
     }
 
-    async getVotesByTopicId(vote_id: number): Promise<Vote[]> {
-        return await this.repository.getVotesFromTopic(vote_id);
+    async getVotesByTopicId(topic_id: number): Promise<VotingResult[]> {
+        if (!topic_id) throw new Error("Topic ID is required");
+
+        const topicIdNum = Number(topic_id);
+        if (isNaN(topicIdNum)) throw new Error("Invalid topic ID");
+
+        return await this.repository.getVotesFromTopic(topicIdNum);
     }
 }
