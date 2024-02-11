dc69e4cb80223e8fbc91853e428b899c3717c560
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-11 01:18:48 -0300

    add vote post route and update route for getting votes from a topic

--- votacao-backend/src/routes/vote-routes.ts
+++ votacao-backend/src/routes/vote-routes.ts
@@ -2,18 +2,31 @@ import { Router } from "express";
 import { VoteRepository } from "../repositories/interfaces/vote-repository";
 import { VoteJsonParser } from "../data-parsers/json/vote-from-json";
 import { VoteController } from "../controllers/vote-controller";
+import { VotePostData } from "../interfaces/vote-post-data";
 
 export const VoteRoutes = (repository: VoteRepository) => {
     const router = Router();
     const parser = new VoteJsonParser();
     const controller = new VoteController(repository);
 
-    router.get('/votes', async (req, res) => {
-        const votingSessions = await controller.getVotesByTopicId(Number(req.query.topic_id));
-        res.status(200).send({
-            "voting_sessions": votingSessions,
-        });
+    router.get('/vote/topic/:topicId', async (req, res) => {
+        const votes = await controller.getVotesByTopicId(Number(req.params.topicId));
+        res.status(200).send(
+            votes,
+        );
     });
+
+    router.post('/vote', async (req, res) => {
+        const vote = req.body as VotePostData
+        console.log(vote);
+        const createdVote = await controller.createVote(vote);
+
+        if (!createdVote) {
+            res.status(500).send("Error creating vote");
+        }
+
+        res.status(201).send(vote);
+    })
 
     return router;
 };
