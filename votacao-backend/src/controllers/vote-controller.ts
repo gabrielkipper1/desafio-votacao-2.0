bb27bc2032bb39f4ae24c9d813f57b99b2ec09b1
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-12 18:11:12 -0300

    protect /vote with  jwt middleware and prevent from voting twice on same topic

--- votacao-backend/src/controllers/vote-controller.ts
+++ votacao-backend/src/controllers/vote-controller.ts
@@ -1,6 +1,8 @@
 import { User } from "../entities/user";
 import { Vote } from "../entities/vote";
 import { VotingTopic } from "../entities/voting-topic";
+import { BadRequestError } from "../exceptions/bad-request-error";
+import { ERRO_MESSAGES } from "../exceptions/erro-messages";
 import { VotePostData } from "../interfaces/vote-post-data";
 import { VotingResult } from "../interfaces/voting-result";
 import { VoteRepository } from "../repositories/interfaces/vote-repository";
@@ -14,6 +16,9 @@ export class VoteController {
 
     async createVote(votePostData: VotePostData): Promise<Vote | undefined> {
         const vote = Vote.create(User.fromId(votePostData.userId), VotingTopic.fromId(votePostData.topicId), votePostData.vote);
+        const hasVotedOnTopic = await this.repository.hasUserVoted(votePostData.topicId, votePostData.userId);
+        if (hasVotedOnTopic) throw new BadRequestError(ERRO_MESSAGES.USER_ALREADY_VOTED);
+
         return this.repository.saveVote(vote);
     }
 
