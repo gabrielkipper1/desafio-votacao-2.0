a7b66fab8b905bb96478f1f57f3c48eeaef0cc0f
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-13 12:22:43 -0300

    create card to show in topic list screen

---
+++ votacao-frontend/src/app/topic-card/topic-card.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { Dialog } from '@angular/cdk/dialog';
+import { Topic } from '../interfaces/topic';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-topic-card',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule, MatDividerModule, MatIconModule, CommonModule],
+  templateUrl: './topic-card.component.html',
+  styleUrl: './topic-card.component.scss'
+})
+export class TopicCardComponent {
+  constructor(private router: Router, private dialog: Dialog) { }
+
+  @Input() topic!: Topic;
+
+  onDetailsButtonClick() {
+    console.log("Details button clicked");
+    console.log(this.topic.id);
+    this.router.navigate(['/', "topic", this.topic.id]);
+  }
+
+  onVoteButtonClick() {
+    console.log("Vote button clicked");
+    console.log(this.topic.id);
+    this.router.navigate(["vote/" + this.topic.id]);
+  }
+
+}
