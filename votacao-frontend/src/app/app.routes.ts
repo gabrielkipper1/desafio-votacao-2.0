97e694eb3f1256ace9d4599221b9caf33fe03e5e
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-13 15:57:36 -0300

    configure routes and intercptors and dependency injection

--- votacao-frontend/src/app/app.routes.ts
+++ votacao-frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { TopicListScreenComponent } from './topic-list-screen/topic-list-screen.component';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { TopicDetailScreenComponent } from './topic-detail-screen/topic-detail-screen.component';
+import { CreateTopicFormComponent } from './create-topic-form/create-topic-form.component';
+import { VoteScreenComponent } from './vote-screen/vote-screen.component';
+import { SignupScreenComponent } from './signup-screen/signup-screen.component';
+import { AppStartComponent } from './app-start/app-start.component';
+import { authGuard } from './guards/auth-guard/auth.guard';
+import { refreshTokenGuard } from './guards/refresh-token-guard/refresh-token.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppStartComponent },
+    { path: 'home', component: TopicListScreenComponent, canActivate: [refreshTokenGuard] },
+    { path: 'login', component: LoginScreenComponent },
+    { path: 'signup', component: SignupScreenComponent },
+    { path: 'topic/:topicId', component: TopicDetailScreenComponent },
+    { path: 'new-topic', component: CreateTopicFormComponent },
+    { path: 'vote/:topicId', canActivate: [authGuard], component: VoteScreenComponent }
+];
