46a1da34b30c9570650ba97668edac1e6b7bd33b
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-13 22:55:24 -0300

    update exceptions thrown by entity constructors

--- votacao-backend/src/entities/user-admin.ts
+++ votacao-backend/src/entities/user-admin.ts
@@ -1,10 +1,13 @@
+import { BadRequestError } from "../exceptions/bad-request-error";
+import { ERROR_MESSAGES } from "../exceptions/erro-messages";
+
 export class UserAdmin {
     active: boolean;
     userId: number;
 
     private constructor(userId: number, active: boolean) {
         if (!userId || !active) {
-            throw new Error("Invalid user or password");
+            throw new BadRequestError(ERROR_MESSAGES.USER_INVALID_DATA);
         }
         this.userId = userId;
         this.active = active;
--- votacao-backend/src/entities/voting-topic.ts
+++ votacao-backend/src/entities/voting-topic.ts
@@ -1,3 +1,5 @@
+import { BadRequestError } from "../exceptions/bad-request-error";
+import { ERROR_MESSAGES } from "../exceptions/erro-messages";
 import { User } from "./user";
 import { Vote } from "./vote";
 import { VotingSession } from "./voting-session";
@@ -23,33 +25,33 @@ export class VotingTopic {
 
     static create(title: string | undefined, category: string | undefined): VotingTopic {
         if (!title || !category) {
-            throw new Error('Invalid input values');
+            throw new BadRequestError(ERROR_MESSAGES.INVALID_TOPIC_DATA);
         }
         return new VotingTopic(undefined, title, category, undefined);
     }
 
     static existing(id: number | undefined, title: string | undefined, category: string | undefined, sessions: VotingSession[] | undefined): VotingTopic {
-        if (!id) throw new Error('Invalid topic Id');
+        if (!id) throw new BadRequestError(ERROR_MESSAGES.INVALID_TOPIC_DATA);
 
         if (!title || !category) {
-            throw new Error('Invalid input values');
+            throw new BadRequestError(ERROR_MESSAGES.INVALID_TOPIC_DATA);
         }
 
         return new VotingTopic(id, title, category, sessions);
     }
 
     static fromId(id: number | undefined): VotingTopic {
-        if (!id) throw new Error('Invalid topic Id');
+        if (!id) throw new BadRequestError(ERROR_MESSAGES.INVALID_TOPIC_DATA);
         return new VotingTopic(id, '', '', undefined);
     }
 
     addVote(vote: Vote) {
         if (!vote.user.id) {
-            throw new Error('Invalid user values');
+            throw new BadRequestError(ERROR_MESSAGES.INVALID_USER_ID);
         }
 
         if (this.votes.has(vote.user.id)) {
-            throw new Error('User cannot vote twice on same topic');
+            throw new BadRequestError(ERROR_MESSAGES.USER_ALREADY_VOTED)
         }
 
         this.votes.set(vote.user.id, vote);
