787a4e61daca9f9764d83cd16fcaefe8471d301a
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-13 22:53:20 -0300

    add error-messages file and log messages on error middleware for better debug experience

--- votacao-backend/src/middlewares/error-middleware.ts
+++ votacao-backend/src/middlewares/error-middleware.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { ApiError } from '../exceptions/api-error';
 
 export function errorHandler(err: Error & Partial<ApiError>, req: Request, res: Response, next: NextFunction) {
+    console.error(err);
     const errorMessage = err.message || 'Internal Server Error';
     res.statusMessage = errorMessage;
     res.status(err.statusCode || 500).send(errorMessage);
