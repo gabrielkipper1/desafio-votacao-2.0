4c0e2b6420593ed4087e6bcae2ac7c80855453f5
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-13 22:59:30 -0300

    update exceptions on jwt encoder and user-from-json parser

--- votacao-backend/src/data-parsers/credentials/jwt-encoder.ts
+++ votacao-backend/src/data-parsers/credentials/jwt-encoder.ts
@@ -1,16 +1,24 @@
 import { TokenEncoder } from "../../interfaces/token-encoder";
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { User } from "../../entities/user";
+import { BadRequestError } from "../../exceptions/bad-request-error";
+import { ERROR_MESSAGES } from "../../exceptions/erro-messages";
 
 export class JWTEncoder<T> implements TokenEncoder {
-    secret: string = "giwqopkropgaqwe";
+    secret: string = process.env.JWT_SECRET || "default-secret";
 
     async encode(payload: object): Promise<string> {
-        return jwt.sign(payload, this.secret, { expiresIn: '1h' });
+        return jwt.sign(payload, this.secret, { expiresIn: "3m" });
     }
 
     async validate(token: string): Promise<object> {
         const decoded = jwt.verify(token, this.secret);
+        if (!decoded) throw new BadRequestError(ERROR_MESSAGES.ERROR_DECODING_TOKEN)
+
+        const decodedPayload = decoded as JwtPayload;
+        if (decodedPayload.exp && (decodedPayload.exp < Date.now() / 1000)) throw new BadRequestError(ERROR_MESSAGES.TOKEN_EXPIRED)
+        if (decodedPayload.iat && (decodedPayload.iat > Date.now() / 1000)) throw new BadRequestError(ERROR_MESSAGES.TOKEN_NOT_YET_VALID)
+
         return decoded as object;
     }
 }
