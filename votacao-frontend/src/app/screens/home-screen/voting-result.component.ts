6a92c3d1ff41d3b1e67771ae751f79636765291e
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-14 18:36:38 -0300

    refactor voting result html, rename functions and add tests

--- votacao-frontend/src/app/screens/home-screen/voting-result.component.ts
+++ votacao-frontend/src/app/screens/home-screen/voting-result.component.ts
@@ -8,11 +8,12 @@ import { CommonModule } from '@angular/common';
 import { VoteResultLineComponent } from '../../components/vote-result-line/vote-result-line.component';
 import { Observable, Subscription, catchError, map, of } from 'rxjs';
 import { VotingOptionsDisplayValues } from '../../interfaces/voting-options-display-values';
+import { LoadingComponent } from '../../components/loading-component/loading.component';
 
 @Component({
   selector: 'app-voting-result',
   standalone: true,
-  imports: [MatCardModule, HttpClientModule, CommonModule, VoteResultLineComponent],
+  imports: [MatCardModule, HttpClientModule, CommonModule, VoteResultLineComponent, LoadingComponent],
   templateUrl: './voting-result.component.html',
   styleUrl: './voting-result.component.scss',
   providers: [HttpClient, VoteService]
@@ -24,6 +25,7 @@ export class VotingResultComponent {
   isActive = false;
   winner: VotingResult | undefined;
   winnerDisplayText: string = "";
+  errorMessage: string | undefined = undefined;
 
   constructor(private voteService: VoteService) { }
 
@@ -31,18 +33,25 @@ export class VotingResultComponent {
     if (this.topic !== undefined && this.topic.id !== undefined) {
       this.votes$ = this.voteService.getVotesByTopicId(this.topic.id).pipe(
         catchError((error) => {
+          this.errorMessage = error.error;
           return of([]);
         }),
         map((votes: VotingResult[]) => {
-          this.totalVotes = votes.reduce((acc, vote) => acc + Number(vote.votes), 0);
+          //this depends on voting information
+          this.calculateTotalVotes(votes);
           this.getWinner(votes);
           return votes;
         }
         )
       );
 
-      this.isSessionActive();
+      //this only depends on topic information
+      this.setActiveSession();
     }
+  }
+
+  calculateTotalVotes(votes: VotingResult[]) {
+    this.totalVotes = votes.reduce((acc, vote) => acc + Number(vote.votes), 0);
   }
 
   getWinner(votes: VotingResult[]) {
@@ -61,9 +70,8 @@ export class VotingResultComponent {
     this.winnerDisplayText = winner ? VotingOptionsDisplayValues[this.winner?.option as keyof typeof VotingOptionsDisplayValues] as string : "Empate";
   }
 
-  isSessionActive() {
+  setActiveSession() {
     this.isActive = this.topic.sessions.filter(
       session => new Date(session.end_date.toString()) > new Date()).length > 0;
-    console.log("this.isActive", this.isActive)
   }
 }
