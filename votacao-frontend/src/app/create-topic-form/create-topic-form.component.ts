31f35ff6ad5e35401697557b5ba392c5ea1e16b0
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-13 21:00:44 -0300

    update create topic screen, including navigation back to home

--- votacao-frontend/src/app/create-topic-form/create-topic-form.component.ts
+++ votacao-frontend/src/app/create-topic-form/create-topic-form.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { TopicService } from '../services/topic-service/topic.service';
 import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardMdImage, MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { showErrorSnackBar, showSuccessSnackBar } from '../helper-functions/show-snack-bar';
+import { getErrorMessage } from '../helper-functions/get-error-message';
 
 @Component({
   selector: 'app-create-topic-form',
   standalone: true,
-  imports: [FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule, HttpClientModule],
+  imports: [MatIconModule, MatCardModule, ReactiveFormsModule, FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule, HttpClientModule, FormsModule],
   templateUrl: './create-topic-form.component.html',
   styleUrl: './create-topic-form.component.scss',
   providers: [TopicService]
@@ -19,17 +25,44 @@ export class CreateTopicFormComponent {
   description!: string;
   category!: string;
   duration!: number;
+  topicForm!: FormGroup;
 
-  constructor(private topicService: TopicService) { }
+  constructor(private topicService: TopicService, private formBuilder: FormBuilder, private router: Router, private snackbar: MatSnackBar) { }
+
+  ngOnInit() {
+    this.topicForm = this.formBuilder.group({
+      description: ['', Validators.required],
+      category: ['', Validators.required],
+      duration: [0, Validators.min(0)]
+    });
+  }
 
   createTopic() {
-    console.log('Creating topic');
     this.topicService.createTopic({
-      description: this.description,
-      category: this.category,
-      durationInMinutes: this.duration
-    }).subscribe(() => {
-      console.log('Topic created');
+      description: this.topicForm.value.description,
+      category: this.topicForm.value.category,
+      durationInMinutes: this.topicForm.value.duration
+    }
+    ).subscribe({
+      next: () => {
+        this.success();
+      },
+      error: (error) => {
+        this.error(error);
+      }
     });
+  }
+
+  success() {
+    showSuccessSnackBar('Pauta criada com sucesso', this.snackbar);
+    this.gotoHome();
+  }
+
+  error(error: Error) {
+    showErrorSnackBar(getErrorMessage(error), this.snackbar);
+  }
+
+  gotoHome() {
+    this.router.navigate(['/home']);
   }
 } 
