695a942609f94b717043a1dbaa87a4b6c3102b7e
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-13 21:04:23 -0300

    make topic detail screen use async pipe and error

--- votacao-frontend/src/app/topic-detail-screen/topic-detail-screen.component.ts
+++ votacao-frontend/src/app/topic-detail-screen/topic-detail-screen.component.ts
@@ -4,36 +4,46 @@ import { Topic } from '../interfaces/topic';
 import { TopicService } from '../services/topic-service/topic.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { SessionTimerComponent } from '../session-timer-component/session-timer.component';
-import { TopicResultComponent } from '../topic-result/topic-result.component';
+import { VotingResultComponent } from '../topic-result/voting-result.component';
 import { LoadingComponent } from '../loading-component/loading.component';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { Observable, catchError, map } from 'rxjs';
 
 @Component({
   selector: 'app-topic-detail-screen',
   standalone: true,
   imports: [
     TopicDetailComponent, SessionTimerComponent, HttpClientModule, MatButtonModule,
-    TopicResultComponent, LoadingComponent, CommonModule, RouterModule, MatIconModule
+    VotingResultComponent, LoadingComponent, CommonModule, RouterModule, MatIconModule
   ],
   providers: [TopicService, HttpClient],
   templateUrl: './topic-detail-screen.component.html',
   styleUrl: './topic-detail-screen.component.scss'
 })
 export class TopicDetailScreenComponent {
+  topic$!: Observable<Topic>;
   topic!: Topic;
+  errorMessage: string | undefined = undefined;
 
   constructor(private topicService: TopicService, private activedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     const topicId = Number(this.activedRoute.snapshot.paramMap.get('topicId'));
-    this.topicService.getTopicAndSessions(topicId).subscribe(
-      topic => {
-        this.topic = topic;
-        console.log('Topic', this.topic);
-      });
+    this.topic$ = this.topicService.getTopicAndSessions(topicId).pipe(
+      catchError((err) => { this.setErrorMessage(err.error); return []; }),
+      // map((topic) => { this.topic = topic; return topic; })
+    );
+  }
+
+  setErrorMessage(message: string) {
+    console.log("error message!");
+    if ('string' !== typeof message) {
+      message = 'Erro desconhecido';
+    }
+    this.errorMessage = message;
   }
 
   gotoHome() {
