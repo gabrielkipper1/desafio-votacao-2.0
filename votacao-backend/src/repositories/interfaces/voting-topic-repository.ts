8d81168f85d26c21a7e6cf2145291b2697b31868
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-15 21:23:51 -0300

    update topic repository to include  "list all" option

--- votacao-backend/src/repositories/interfaces/voting-topic-repository.ts
+++ votacao-backend/src/repositories/interfaces/voting-topic-repository.ts
@@ -4,6 +4,6 @@ import { VotingTopicPostData } from "../../interfaces/voting-topic-post-data";
 
 export interface VotingTopicRepository {
     createVotingTopic(votingTopic: VotingTopic): Promise<VotingTopic>;
-    getActiveVotingTopics(search: TopicSearchData): Promise<VotingTopic[]>;
+    getActiveVotingTopics(search: TopicSearchData, listAll: boolean): Promise<VotingTopic[]>;
     getVotingTopicById(id: number): Promise<VotingTopic | undefined>;
 }
