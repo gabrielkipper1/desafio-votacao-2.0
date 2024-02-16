039d5e99b3f29cf25452a1d293a82e3a833b4ea0
Gabriel Limberger <g.kipper@hotmail.com>
2024-02-15 21:14:11 -0300

    setup admin guard and fix refresh token guard

--- votacao-frontend/src/app/guards/refresh-token-guard/refresh-token.guard.ts
+++ votacao-frontend/src/app/guards/refresh-token-guard/refresh-token.guard.ts
@@ -8,10 +8,11 @@ export const refreshTokenGuard: CanActivateFn = (route, state) => {
   const authService: AuthService = inject(AuthService);
   const token = authService.getToken();
 
+  //this guard does not prevente the user from accessing the route
+  //it only checks if the token is still valid and if not, it will log the user out
   if (token === undefined || token.token === undefined || token.token === "") {
-    return of(false);
+    return of(true);
   }
-
 
   return authService.validate(token.token).pipe(
     catchError((error) => {
